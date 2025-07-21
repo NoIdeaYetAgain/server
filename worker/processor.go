bdd37d9d8fe9e9c061d1195d97c125556d5a060b
Elizabeth <elizabethogundepo7@gmail.com>
2025-07-21 17:59:02 +0100

    feat: add asynq worker error logger

--- worker/processor.go
+++ worker/processor.go
@@ -4,6 +4,7 @@ import (
 	db "clove/internal/db/sqlc"
 	"context"
 	"github.com/hibiken/asynq"
+	"github.com/rs/zerolog/log"
 )
 
 const (
@@ -29,6 +30,10 @@ func NewRedisTaskProcessor(redisOpt asynq.RedisClientOpt, store db.SQLStore) Tas
 				QueueCritical: 10,
 				QueueDefault:  5,
 			},
+			ErrorHandler: asynq.ErrorHandlerFunc(func(ctx context.Context, task *asynq.Task, err error) {
+				log.Error().Err(err).Str("type", task.Type()).Bytes("payload", task.Payload()).Msg("process task failed")
+			}),
+			Logger: NewLogger(),
 		},
 	)
 
